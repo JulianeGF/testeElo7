5ce749a4cc6cce46c7f46c430b6d262fc9ee743a
julianegf <julianeferreira@me.com>
2020-11-25 22:03:19 -0300

    Starting Test

---
+++ buscaNativaElo7/buscaNativaElo7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  buscaNativaElo7
+//
+//  Created by shu on 25/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
