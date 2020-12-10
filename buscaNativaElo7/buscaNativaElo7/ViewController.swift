95642935c5c62a285770984b85701b24b3e99c64
julianegf <julianeferreira@me.com>
2020-12-09 22:37:44 -0300

    alamofire request

--- buscaNativaElo7/buscaNativaElo7/ViewController.swift
+++ buscaNativaElo7/buscaNativaElo7/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchView()
     }
-
+    private func searchView() {
+        let request = Alamofire.request("https://5dc05c0f95f4b90014ddc651.mockapi.io/elo7/api/1/products")
+            
+            request.responseJSON { (data) in
+              print(data)
+            }
+    }
 
 }
 
