c41e0b9b743e4addc50f4ccba14a2dd212b75dbf
julianegf <julianeferreira@me.com>
2020-12-13 16:51:54 -0300

    still studying how to make work the tableView

---
+++ buscaNativaElo7/buscaNativaElo7/ProductTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  ProductTableViewCell.swift
+//  buscaNativaElo7
+//
+//  Created by shu on 12/12/20.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var productCell: UIView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- buscaNativaElo7/buscaNativaElo7/ViewController.swift
+++ buscaNativaElo7/buscaNativaElo7/ViewController.swift
@@ -4,23 +4,43 @@
 //
 //  Created by shu on 25/11/20.
 //
-
+import Foundation
 import UIKit
 import Alamofire
 
+
 class ViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         searchView()
     }
+    
     private func searchView() {
         let request = Alamofire.request("https://5dc05c0f95f4b90014ddc651.mockapi.io/elo7/api/1/products")
             
-            request.responseJSON { (data) in
-              print(data)
-            }
+        request.responseJSON { (data) in
+            print(data)
+        }
+            
     }
-
 }
 
+struct price: Codable{
+    let current: String
+    let installment: String
+    let nonPromotional: String
+}
+struct product: Codable {
+    let _link: String
+    let picture: String
+    let title: String
+}
+enum CodingKeys: String, Codable, CodingKey {
+    case price
+    case _link
+    case picture
+    case title
+    }
